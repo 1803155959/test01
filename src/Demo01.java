360fe4359ce1f9c90457cf21ceea03a9be8c9e54
1803155959 <1803155959@qq.com>
2023-08-28 09:18:15 +0800

    第一次提交，添加了以Demo01.java文件，完成了helloword的打印输出

---
+++ src/Demo01.java
@@ -0,0 +1,12 @@
+/**
+ * @ClassName Demo01
+ * @Description TODO
+ * @Author daiyigong
+ * @Date 2023/8/28 9:05
+ * @Version 1.0
+ */
+public class Demo01 {
+    public static void main(String[] args) {
+        System.out.println("Demo01");
+    }
+}
